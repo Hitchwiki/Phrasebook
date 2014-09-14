256706aef9bd4b1ff605df6a7936df77f92003e6
Mikael Korpela <mikael@ihminen.org>
2014-09-14 18:26:44 +0300

    #20 Prepare for market links for different environments
    http://developer.android.com/distribute/tools/promote/linking.html#andro
    id-app
    https://github.com/apache/cordova-plugin-device/blob/master/doc/index.md

--- src/js/controllers/about.js
+++ src/js/controllers/about.js
@@ -18,4 +18,15 @@ Phrasebook.controller('aboutCtrl', function($scope) {
         $location.path('/');
     }
 
+
+    /*
+     * Different link depending on environment
+     * @link http://developer.android.com/distribute/tools/promote/linking.html#android-app
+     * @link https://github.com/apache/cordova-plugin-device/blob/master/doc/index.md
+     * @todo
+     */
+    $scope.linkAppleApp = 'https://itunes.apple.com/us/app/hitchwiki.org-phrasebook/id679023494?ls=1&mt=8';
+    $scope.linkAndroidApp = 'https://play.google.com/store/apps/details?id=com.hitchwiki.phrasebook';
+    $scope.linkAndroidSearch = 'https://play.google.com/store/search?q=hitchwiki';
+
 });
